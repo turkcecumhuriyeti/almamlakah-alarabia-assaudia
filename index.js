c224b6e57c969500090c2d2179184fe510af2257
turkcecumhuriyeti <yecheolseo@gmail.com>
2025-01-08 20:35:41 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
-let a[];
-async function start();
-{
+let a=[];
+async function start(){
     const response = await fetch(`https://sheets.googleapis.com/v4/spreadsheets/1DqsLYRqCm0JJQ64l47gzn309h_dPHLAE8XI57rVh_Ng/values/시트1?key=AIzaSyDccz-Qh9fa66POleHS0P7htB3NpVOz0Qg`);
     const data = await response.json();
     a = data.values;
