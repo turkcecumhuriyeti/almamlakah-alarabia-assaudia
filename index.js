70b9bd74ea00897af8e57d34bb6a0395aab86050
turkcecumhuriyeti <yecheolseo@gmail.com>
2025-01-09 01:08:29 +0900

    index.js 업데이트

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 let a=[];
+document.querySelector('input').oninput = search;
 async function start(){
     const response = await fetch(`https://sheets.googleapis.com/v4/spreadsheets/1DqsLYRqCm0JJQ64l47gzn309h_dPHLAE8XI57rVh_Ng/values/시트1?key=AIzaSyDccz-Qh9fa66POleHS0P7htB3NpVOz0Qg`);
     const data = await response.json();
@@ -9,7 +10,6 @@ start();
 
 function search(){
     const inp = document.querySelector('input').value;
-    document.querySelector('input').oninput = search;
     a.forEach(element=>{
         if(element[0].includes(inp) || element[1].includes(inp){
             document.writeln(`<h2>${element[0]}</h2><div>${element[1]}</div>`);
